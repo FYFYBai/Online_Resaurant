223288655c55c6836c51f9d0b8952e6e0d9e41dc
FYFYBai <qihe2000@foxmail.com>
2025-02-24 17:32:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,49 +7,49 @@ app.use(express.json());
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Import restaurant routes
-const restaurantRoutes = require('./app/routes/restaurant');
-app.use('/api', restaurantRoutes);
+const componentRoutes = require('./app/routes/component.route');
+app.use('/api', componentRoutes);
 
 // Import the Sequelize instance from your configuration
 const sequelize = require('./app/config/database');
 require('./app/models/index')
 // Synchronize the database and then start the server
 sequelize.sync()
-    .then(() => {
-        console.log('Database synchronized.');
-        const port = process.env.PORT || 3000;
-        app.listen(port, () => {
-            console.log(`Server running on port ${port}`);
-        });
-    })
-    .catch(error => {
-        console.error('Error synchronizing database:', error);
+  .then(() => {
+    console.log('Database synchronized.');
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
     });
+  })
+  .catch(error => {
+    console.error('Error synchronizing database:', error);
+  });
 
-    const calculateOrderAmount = (items) => {
-        // Calculate the order total on the server to prevent
-        // people from directly manipulating the amount on the client
-        let total = 0;
-        items.forEach((item) => {
-          total += item.amount;
-        });
-        return total;
-      };
-      
-      app.post("/create-payment-intent", async (req, res) => {
-        const { items } = req.body;
-      
-        // Create a PaymentIntent with the order amount and currency
-        const paymentIntent = await stripe.paymentIntents.create({
-          amount: calculateOrderAmount(items),
-          currency: "cad",
-          // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
-          automatic_payment_methods: {
-            enabled: true,
-          },
-        });
-      
-        res.send({
-          clientSecret: paymentIntent.client_secret,
-        });
-      });
+const calculateOrderAmount = (items) => {
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  let total = 0;
+  items.forEach((item) => {
+    total += item.amount;
+  });
+  return total;
+};
+
+app.post("/create-payment-intent", async (req, res) => {
+  const { items } = req.body;
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "cad",
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
