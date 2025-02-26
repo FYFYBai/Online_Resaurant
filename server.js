681869f61aab190b187965d56c30e276b8defa9f
FYFYBai <qihe2000@foxmail.com>
2025-02-25 21:32:53 -0500

    Update server.js

--- server.js
+++ server.js
@@ -69,60 +69,60 @@ sequelize.sync()
     console.error('Error synchronizing database:', error);
   });
 
-const calculateOrderAmount = (items) => {
-  // Calculate the order total on the server to prevent
-  // people from directly manipulating the amount on the client
-  let total = 0;
-  items.forEach((item) => {
-    total += item.amount;
-  });
-  return total;
-};
+// const calculateOrderAmount = (items) => {
+//   // Calculate the order total on the server to prevent
+//   // people from directly manipulating the amount on the client
+//   let total = 0;
+//   items.forEach((item) => {
+//     total += item.amount;
+//   });
+//   return total;
+// };
 
-app.post("/create-payment-intent", async (req, res) => {
-  const { items } = req.body;
+// app.post("/create-payment-intent", async (req, res) => {
+//   const { items } = req.body;
 
-  // Create a PaymentIntent with the order amount and currency
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: calculateOrderAmount(items),
-    currency: "cad",
-    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
-    automatic_payment_methods: {
-      enabled: true,
-    },
-  });
+//   // Create a PaymentIntent with the order amount and currency
+//   const paymentIntent = await stripe.paymentIntents.create({
+//     amount: calculateOrderAmount(items),
+//     currency: "cad",
+//     // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+//     automatic_payment_methods: {
+//       enabled: true,
+//     },
+//   });
 
 
-    const calculateOrderAmount = (items) => {
-        // Calculate the order total on the server to prevent
-        // people from directly manipulating the amount on the client
-        let total = 0;
-        items.forEach((item) => {
-          total += item.amount;
-        });
-        return total;
-      };
+//     const calculateOrderAmount = (items) => {
+//         // Calculate the order total on the server to prevent
+//         // people from directly manipulating the amount on the client
+//         let total = 0;
+//         items.forEach((item) => {
+//           total += item.amount;
+//         });
+//         return total;
+//       };
       
-      app.post("/create-payment-intent", async (req, res) => {
-        const { items } = req.body;
+//       app.post("/create-payment-intent", async (req, res) => {
+//         const { items } = req.body;
       
-        // Create a PaymentIntent with the order amount and currency
-        const paymentIntent = await stripe.paymentIntents.create({
-          amount: calculateOrderAmount(items),
-          currency: "cad",
-          // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
-          automatic_payment_methods: {
-            enabled: true,
-          },
-        });
+//         // Create a PaymentIntent with the order amount and currency
+//         const paymentIntent = await stripe.paymentIntents.create({
+//           amount: calculateOrderAmount(items),
+//           currency: "cad",
+//           // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+//           automatic_payment_methods: {
+//             enabled: true,
+//           },
+//         });
       
-        res.send({
-          clientSecret: paymentIntent.client_secret,
-        });
-      });
+//         res.send({
+//           clientSecret: paymentIntent.client_secret,
+//         });
+//       });
 
-  res.send({
-    clientSecret: paymentIntent.client_secret,
-  });
-});
+//   res.send({
+//     clientSecret: paymentIntent.client_secret,
+//   });
+// });
 
